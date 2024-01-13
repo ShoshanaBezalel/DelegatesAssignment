4b37f8d162eff702e7cccd5b9aac54994a89e838
ShoshanaBezalel <91996753+ShoshanaBezalel@users.noreply.github.com>
2024-01-13 21:31:59 +0200

    Add project files.

---
+++ DelegatesAssignment/Positives.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAssignment
+{
+    internal class Positives
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfGettingVirus { get; set; }
+        public DateTime DateOfTest { get; set; }
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - DateOfBirth.Year;
+            }
+        }
+        public string VariantType { get; set; }
+
+
+    }
+}
